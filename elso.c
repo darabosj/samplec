149465075dfc39584afd8baf6779869c2602458e
jdarabos <darabosj@gmail.com>
2016-11-29 12:19:03 +0100

    username

--- elso.c
+++ elso.c
@@ -2,14 +2,25 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
+
+void info(){
+	char *uname;
+	uname = getlogin();
+	if (!uname){
+	printf("uname error! %i \n ",errno);
+	} else{
+	printf("Login name :%s\n",getlogin());}
+	printf("Pid : %i \n",getpid());
+
+}
 
 int main(int argc, char **argv) {
 	int i;
 	char nev[30];
 	pid_t p;
-
-	printf("Pid : %i ",getpid());
-
+	
+	info();
 	if (argc<2){
 	printf("Invalid parameter! \n");
 	return 1;
