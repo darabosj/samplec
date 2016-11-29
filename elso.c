a389f55356d70b91515efb5d88eef5540be5127b
jdarabos <darabosj@gmail.com>
2016-11-29 11:49:31 +0100

    getpid added

--- elso.c
+++ elso.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
 	int i;
 	char nev[30];
+	pid_t p;
+
+	printf("Pid : %i ",getpid());
 
 	if (argc<2){
 	printf("Invalid parameter! \n");
